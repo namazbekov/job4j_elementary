0d8531ee54499876151a25e70e6d2a38c3afa66a
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-04-11 13:03:41 +0300

    3.4. Заголовок в файле java [#511724]

--- src/ru/job4j/Multiple.java
+++ src/ru/job4j/Multiple.java
@@ -4,6 +4,11 @@ public class Multiple {
     public static void main(String[] args) {
         System.out.println(1*2);
         System.out.println(1*3);
+        System.out.println(1*4);
+        System.out.println(1*5);
+        System.out.println(1*6);
+        System.out.println(1*7);
+        System.out.println(1*8);
         System.out.println(1*9);
     }
 }
