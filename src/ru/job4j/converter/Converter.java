34aade7c77ed23a0adc7272797773dc2eba32fe2
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-04-13 20:39:46 +0300

    5. Конвертер валюты [#511702]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -4,13 +4,13 @@ public class Converter {
 
 
         public static int rubleToEuro(int value) {
-            int rsl = value / 70;
-            return rsl;
+
+            return value / 70;
         }
 
         public static int rubleToDollar(int value) {
-            int rsl = value / 60; /* формула перевода рублей в доллары. */
-            return rsl;
+
+            return value / 60;
         }
 
         public static void main(String[] args) {
