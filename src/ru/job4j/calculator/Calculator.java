3ff18089d7d95712259373aa938b97c6d744ad74
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-04-14 11:50:55 +0300

    4.1. Аргументы [#511714]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -3,14 +3,14 @@ package ru.job4j.calculator;
 public class Calculator {
 
     public static void hello(String name) {
-        int age = 33;
-        System.out.println("Hello, " + name + age);
+        System.out.println("Hello, " + name);
     }
 
     public static void main(String[] args) {
         String name = "Petr Arsentev ";
+        int age = 33;
         Calculator.hello(name);
+        System.out.println(age);
 
     }
-
 }
