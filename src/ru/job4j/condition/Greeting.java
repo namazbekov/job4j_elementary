ca4fc6ed8e755be75c5697109a1538bab8c07ba5
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-04-15 12:00:13 +0300

    9.1. String [#511713]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java! ";
+        System.out.println(idea);
+        String word = "But I am a newbie.";
+        String ideaWord = idea + word;
+        int year = 2021;
+        String all = ideaWord + year;
+        System.out.println(all);
+    }
+}
