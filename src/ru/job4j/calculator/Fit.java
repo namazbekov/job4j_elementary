a9092539ca67420b084d19db905a4d4bb22bdf92
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-04-17 21:55:34 +0300

    6. Идеальный вес [#511703]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -4,13 +4,10 @@ public class Fit {
 
     public static double manWeight(short height) {
         return (height - 100) * 1.15;
-
     }
 
-    @SuppressWarnings("checkstyle:EmptyLineSeparator")
     public static double womanWeight(short height1) {
         return (height1 - 110) * 1.15;
-
     }
 
     public static void main(String[] args) {
