e69924b05adaf9ea746d5880098b96679b841b75
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-04-22 13:45:07 +0300

    вывести максимальное значение из 3 чисел

--- src/MaxNumber.java
+++ src/MaxNumber.java
@@ -1,19 +1,16 @@
 public class MaxNumber {
 
-    public static void Art(int A, int B, int C) {
+    public static void main(String[] args) {
+        MaxNumber.Args(10,2,5);
+    }
 
-        if (A>B) {
-            System.out.println(A);
+    public static void Args(int A, int B, int C) {
+        if (A > B) {
+            if (A > C)
+                System.out.println(A);
         } else {
             System.out.println(B);
         }
-        if (A>C){
-            System.out.println(A);
-        } else {
-            System.out.println(C);
-        }
     }
-    public static void main(String[] args) {
-        MaxNumber.Art(10,5,2);
-    }
+
 }
