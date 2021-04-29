955b2b9a30cbca6e5c981c77e800ca1e747d0636
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-04-29 21:59:38 +0300

    5.4. Протеиновая диета [#511761]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -4,7 +4,6 @@ public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
         while (ivan <= nik) {
-            System.out.println();
             ivan *= 3;
             nik *= 2;
             month++;
