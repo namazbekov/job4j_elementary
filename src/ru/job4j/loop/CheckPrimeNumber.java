9174702979290066193e1021161b93a526c77857
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-04-30 11:24:06 +0300

    5.5. Простое число [#511764]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,22 +2,15 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        for (int i = 0; i <= number; i++) {
+        boolean prime = number > 1;
+        for (int i = 2; i < number; i++) {
             if (number % 2 == 0) {
-                System.out.println("не явлется простым числом");
                 prime = false;
-                break;
-            } else {
-                System.out.println("является простым числом");
                 break;
             }
         }
         return prime;
     }
 
-    public static void main(String[] args) {
-        System.out.println(check(5));
-    }
 }
 
---
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public static int calc(int finish) {
+        int count = 0;
+        for (int number = 1; number <= finish; number++) {
+            if (CheckPrimeNumber.check(number)) {
+                count++;
+                break;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calc(5));
+    }
+}
+
